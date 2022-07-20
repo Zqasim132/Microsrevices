c4481ec0462d8f04cb26c025270c1ea5214feb4d
Zeeshan Qasim <zqasim132@gmail.com>
2022-07-20 13:09:50 +0500

    20/07/2022 added the RabbitMQ Test application into the GitHub Repo.

---
+++ RabitMQ/RabbitMQ_consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+IConnection connection;
+IModel channel;
+ConnectionFactory connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    VirtualHost = "/",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+connection = connectionFactory.CreateConnection();
+channel = connection.CreateModel();
+
+//for setting the messages according to their priority
+channel.BasicQos(0,1,false);
+
+//creating consumer object
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+//attaching the function to event handler
+consumer.Received += (sender, args) => {
+    string message = Encoding.UTF8.GetString(args.Body.Span);
+    Console.WriteLine("Message is: {0}",message);
+    channel.BasicAck(args.DeliveryTag,false);
+};
+
+var consumerTag = channel.BasicConsume("Queue_2",false, consumer);
+
+Console.WriteLine("Waiting for messages. Press any key to exit. ");
+Console.ReadKey();
---
+++ RabitMQ/RabitMQ/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+IConnection connection;
+IModel channel;
+ConnectionFactory connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    VirtualHost = "/",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+connection = connectionFactory.CreateConnection();
+channel = connection.CreateModel();
+
+
+//Exchange Creation
+channel.ExchangeDeclare("ex.fanout", "fanout", true, false, null);
+
+//Queues creation
+channel.QueueDeclare("Queue_1",true,false,false, null);
+channel.QueueDeclare("Queue_2",true,false,false, null);
+
+//Queues binding
+channel.QueueBind("Queue_1", "ex.fanout", "", null);
+channel.QueueBind("Queue_2", "ex.fanout", "", null);
+
+//Publishing message
+channel.BasicPublish("ex.fanout", "", null, Encoding.UTF8.GetBytes("First Message..!"));
+channel.BasicPublish("ex.fanout", "", null, Encoding.UTF8.GetBytes("Second Message..!"));
+
+
+////deleting
+//channel.QueueDelete("Queue_1");
+//channel.QueueDelete("Queue_2");
+//channel.ExchangeDelete("ex.fanout");
+
+////closing
+//channel.Close();
+//connection.Close();
